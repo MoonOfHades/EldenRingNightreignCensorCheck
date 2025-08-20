549489bcf9cd4334d13bda0683f2dc3e1bbfc183
SirAnsbach <siransbach@users.noreply.github.com>
2025-08-20 02:28:17 -0700

    Add in-development notice

--- eldenringnamechecker/src/App.tsx
+++ eldenringnamechecker/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          (Site in development)
         </a>
       </header>
     </div>
